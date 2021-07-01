496b442015e48da284600afff301fdb70c2a193a
bharatasawale <bharatasawale@gmail.com>
2021-07-01 23:08:20 +0530

    follow request change to post

--- src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/FollowController.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/FollowController.java
@@ -1,14 +1,12 @@
 package com.bezkoder.spring.jwt.mongodb.controllers;
 
 import com.bezkoder.spring.jwt.mongodb.models.User;
+import com.bezkoder.spring.jwt.mongodb.payload.request.FollowRequest;
 import com.bezkoder.spring.jwt.mongodb.payload.response.MessageResponse;
 import com.bezkoder.spring.jwt.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,5 +65,48 @@ public class FollowController {
             return ResponseEntity.badRequest().body(new MessageResponse("User not found.."));
     }
 
+    @PostMapping(value = "/")
+    public ResponseEntity followPostRequest(@RequestBody FollowRequest followRequest){
+        if(userRepository.existsById(followRequest.getFollower())){
+            try{
+                User user=userRepository.findUserById(followRequest.getFollower());
+                List<String> following = user.getFollowing();
+                if(following.contains(followRequest.getFollowing())){
+                    return ResponseEntity.badRequest().body(new MessageResponse("already following"));
+                }
+                else {
+                    following.add(followRequest.getFollowing());
+                    user.setFollowing(following);
+                    userRepository.save(user);
+                    User user1 = userRepository.findUserById(followRequest.getFollowing());
+                    List<String> follower = user1.getFollower();
+                    if(follower.contains(followRequest.getFollower())){
+                        return ResponseEntity.badRequest().body(new MessageResponse("already follower"));
+                    }
+                    else {
+                        follower.add(followRequest.getFollower());
+                        user1.setFollower(follower);
+                        userRepository.save(user1);
+                        return ResponseEntity.ok().body(new MessageResponse("done"));
+                    }
+                }
+            }
+            catch (NullPointerException e){
+                User user = userRepository.findUserById(followRequest.getFollower());
+                List<String> following = new ArrayList<>();
+                following.add(followRequest.getFollowing());
+                user.setFollowing(following);
+                userRepository.save(user);
+                User user1=userRepository.findUserById(followRequest.getFollowing());
+                List<String> follower = new ArrayList<>();
+                follower.add(followRequest.getFollower());
+                user1.setFollower(follower);
+                userRepository.save(user1);
+                return ResponseEntity.ok().body(new MessageResponse("done"));
+            }
+        }
+        else
+            return ResponseEntity.badRequest().body(new MessageResponse("User not found.."));
+    }
 
 }
