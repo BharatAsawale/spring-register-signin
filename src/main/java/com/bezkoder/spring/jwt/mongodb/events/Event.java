78c730fa0d9c11ba7073620df586becf1632f9db
bharatasawale <bharatasawale@gmail.com>
2022-02-07 23:39:31 +0530

    event date added

--- src/main/java/com/bezkoder/spring/jwt/mongodb/events/Event.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/events/Event.java
@@ -1,8 +1,10 @@
 package com.bezkoder.spring.jwt.mongodb.events;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalTime;
 import java.util.Date;
 
 @Data
@@ -12,7 +14,12 @@ public class Event {
     private String organizer;
     private String details;
     private int amount;
-    private Date date;
+    @JsonFormat(pattern = "dd/mm/yyyy")
+    private Date startDate;
+    @JsonFormat(pattern = "dd/mm/yyyy")
+    private Date endDate;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime startTime;
     private String contact;
     private String email;
     private String url;
